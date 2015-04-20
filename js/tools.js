ea8473be99786e2c334fbf9fc86b963b72bf2f1b
sebastianquinterozuluaga <sebastian.quintero@payulatam.com>
2015-04-20 16:06:44 -0500

    hi title shrink - js tools

---
+++ js/tools.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+	var waypoint = new Waypoint({
+		element: document.getElementById("content"),
+		handler: function(direction) {
+
+			if (direction === 'down') {
+				$(".payu-title1").addClass("collapsed");    
+			} else {
+				$(".payu-title1").removeClass("collapsed");    
+			}
+
+		},
+		offset: (  69 ) * -1
+	});
+
+}
