31076fc4606f8cfde2cc7e63c79d7c090e5dc510
AlvinDiaz <alvin.diaz@payulatam.com>
2016-04-07 09:12:28 -0500

    Calculadora de firmas
    se cambia tipo de input de numeric a texto y se añade validacion manual
    de solo números por jquery **** se modifican estilos para mejorar
    vusualización en firefox

--- js/signature-generator/validations.js
+++ js/signature-generator/validations.js
@@ -1,5 +1,18 @@
 $(function() {
-
+    
+    function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        evt.preventDefault();
+        return false;
+    }
+    return true;
+   }
+   
+   $(".number").keypress(function(e){     
+       isNumber(e);
+   });
     
   $.validator.addMethod('positiveNumber',
     function (value) { 
