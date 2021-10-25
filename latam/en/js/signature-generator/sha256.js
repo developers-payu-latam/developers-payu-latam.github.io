b4df9c3cb3d00187489cc79261d27a72959ff027
JorgeR1986 <81313045+JorgeR1986@users.noreply.github.com>
2021-10-25 10:32:39 -0500

    URL fixing

--- latam/en/js/signature-generator/sha256.js
+++ latam/en/js/signature-generator/sha256.js
@@ -255,10 +255,3 @@ var CryptoJS = CryptoJS || function(h, s) {
     s.SHA256 = g._createHelper(f);
     s.HmacSHA256 = g._createHmacHelper(f)
 })(Math);
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
-}
