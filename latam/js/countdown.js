af6b87e785cd41428512a79a133aa231bd0c08d2
JorgeR1986 <81313045+JorgeR1986@users.noreply.github.com>
2022-05-05 14:20:49 -0500

    Update countdown.js

--- latam/js/countdown.js
+++ latam/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 30, 2022 00:00:00").getTime();
+var countDownDate = new Date("May 21, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
